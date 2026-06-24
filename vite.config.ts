f7c6417f96a508cc3a21bfd77a75226f0228432e
flash12242002 <flash12242002@gmail.com>
2026-06-24 10:24:35 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/ldconboardingportal/'
+    base:'/ldconboardingportal/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
